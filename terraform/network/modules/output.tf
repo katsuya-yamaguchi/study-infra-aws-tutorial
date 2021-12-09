1c226fd856b0d6bbc8a073a359bc30756550837c
katsuya yamaguchi <ka.yamaguchi.3@gmail.com>
2021-12-09 23:33:30 +0900

    #8 publicサブネットをoutputに追加。

--- terraform/network/modules/output.tf
+++ terraform/network/modules/output.tf
@@ -1,3 +1,10 @@
 output "vpc_id" {
   value = aws_vpc.study_infra.id
 }
+
+output "public_subnets_id" {
+  value = [
+    aws_subnet.public["public_a"].id,
+    aws_subnet.public["public_c"].id
+  ]
+}
