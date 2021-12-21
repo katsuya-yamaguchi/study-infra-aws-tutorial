961310ba818bbe4af899cb8180ff8911b29b062d
katsuya yamaguchi <ka.yamaguchi.3@gmail.com>
2021-12-21 15:19:55 +0900

    #15 動的ポートマッピングに対応するようにセキュリティグループのルールを修正。

--- terraform/system/modules/web/ec2_sg.tf
+++ terraform/system/modules/web/ec2_sg.tf
@@ -21,8 +21,10 @@ resource "aws_security_group_rule" "ecs_container_instance_to_outbound" {
 resource "aws_security_group_rule" "ecs_container_instance_from_alb_web" {
   type                     = "ingress"
   security_group_id        = aws_security_group.ecs_container_instance_web.id
-  from_port                = 80
-  to_port                  = 80
+  # ECSのnetwork modeはdefaultを使用し、動的ポートマッピングも使用する。
+  # https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/task_definition_parameters.html#container_definitions
+  from_port                = 49153
+  to_port                  = 65535
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.alb_web.id
 }
